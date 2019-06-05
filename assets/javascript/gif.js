72e44c8283c299a279302e21da95844324062113
Your Name <cory.mcshea@gmail.com>
2019-06-04 22:37:57 -0400

    adding div and img tags

--- assets/javascript/gif.js
+++ assets/javascript/gif.js
@@ -3,26 +3,39 @@ $(document).ready(function () {
 //vars for array of buttons
 var topics = ['Babies','Puppies','Kittens','Ponies', 'Ducklings', 'Bambi', 'Chicks','Pigs','Shrek'];
 
+//adding click event listener
+$("#buttons").on("click", function () {
+  var baby = $(this).attr("data-name");
+  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + baby +
+    "&api_key=2LWuyvR2In1qc9st20gT4JCWDe2hdENT&limit=10";
 
-    //on click event
-    $("#buttons").on("click", function () {
-        var baby = $(this).attr("data-name");
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + baby +
-            "&api_key=2LWuyvR2In1qc9st20gT4JCWDe2hdENT&limit=10";
+  //performing an AJAX request with queryURL
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+    })
+      .then(function (response) {
+        console.log(queryURL);
+    
+    //storing the data from the AJAX requesting in the results var
+    var results = response.data;
 
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).then(function (response) {
-            console.log(queryURL);
+    //for loop
+    for (var i = 0; i < results.length; i++) {
 
-            var results = response.data;
+        //Creating a div tag
+        var babyDiv = $("<div>");
 
-            //for loop
-            for (var i = 0; i < results.length; i++) {
-                console.log(results);
-            };
-        });
+        //Creating a paragraph tag with the rating
+        var p = $("<p>").text("Rating: " + results[i].rating);
+        
+        //Creating and storing an img tag
+        var babyImage = $("<img>");
+        
 
+      console.log(results);
+
+      };
     });
+  });
 });
