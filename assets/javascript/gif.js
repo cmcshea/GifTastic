6d0e03d9ac176d5a5bce10976be6213cbdfbc299
Your Name <cory.mcshea@gmail.com>
2019-06-04 21:02:14 -0400

    jquery additions

--- assets/javascript/gif.js
+++ assets/javascript/gif.js
@@ -1,4 +1,11 @@
 
-$("#buttons").on("click", function(){
-console.log(event)
+//vars for the ajax search
+
+//on click event
+$("#buttons").on("click", function () {
+  var baby = $(this).attr("#data-name");
+  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + baby + 
+  "&api_key=2LWuyvR2In1qc9st20gT4JCWDe2hdENT&limit=10";
+
+  console.log(this)
 });
