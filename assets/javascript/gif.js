5848c7b4c77159ba1ff14a9c64443673d0d770ad
Your Name <cory.mcshea@gmail.com>
2019-06-04 22:15:41 -0400

    topics array

--- assets/javascript/gif.js
+++ assets/javascript/gif.js
@@ -1,24 +1,28 @@
+$(document).ready(function () {
 
-//vars for the ajax search
+//vars for array of buttons
+var topics = ['Babies','Puppies','Kittens','Ponies', 'Ducklings', 'Bambi', 'Chicks','Pigs','Shrek'];
 
-//on click event
-$("#buttons").on("click", function () {
-  var baby = $(this).attr("data-name");
-  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + baby + 
-  "&api_key=2LWuyvR2In1qc9st20gT4JCWDe2hdENT&limit=10";
 
-  $.ajax({
-    url: queryURL,
-    method: "GET"
-  }).then(function(response) {
-    console.log(response);
+    //on click event
+    $("#buttons").on("click", function () {
+        var baby = $(this).attr("data-name");
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + baby +
+            "&api_key=2LWuyvR2In1qc9st20gT4JCWDe2hdENT&limit=10";
 
-    var results = response.data;
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            console.log(queryURL);
 
-//for loop
-for (var i =0; i < results.length; i++) {
-console.log(results);
-};
-  });
- 
+            var results = response.data;
+
+            //for loop
+            for (var i = 0; i < results.length; i++) {
+                console.log(results);
+            };
+        });
+
+    });
 });
